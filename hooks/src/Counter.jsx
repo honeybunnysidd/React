f22a180b3c38648cc7acf356623d6f091e6fbc34
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-04-25 06:02:12 +0530

    useEffect

---
+++ hooks/src/Counter.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function Counter() {
+  let [countx, setCountx] = useState(0);
+  let [county, setCounty] = useState(0);
+  let incCountx = () => {
+    setCountx((preValue) => {
+      return ++preValue;
+    });
+  };
+  let incCounty = () => {
+    setCounty((preValue) => {
+      return ++preValue;
+    });
+  };
+
+  useEffect(function print() {
+    console.log("this is use Effect");
+  }, []);
+
+  return (
+    <div>
+      <h2>Counter {countx}</h2>
+      <button onClick={incCountx}>Count++</button>
+
+      <br />
+      <br />
+      <h2>Counter {county}</h2>
+      <button onClick={incCounty}>Count++</button>
+    </div>
+  );
+}
